b4e54544ca71bd7bfec33b05573b94aa4d597b8f
lernado <leonardo.pdr.lopes@outlook.com>
2017-11-11 04:54:32 -0200

    Update merda.py
    agora esta funcionando .

--- merda.py
+++ merda.py
@@ -1,71 +1,72 @@
 import pygame, sys
 from pygame.locals import *
 
-largura= 900
-altura=400
+largura= 1200
+altura=700
 
-class Bala(pygame.sprite.Sprite):
+class bala(pygame.sprite.Sprite):
     def __init__(self, posx, posy):
         pygame.sprite.Sprite.__init__(self)
-        self.imagemBala = pygame.image.load("imagem/bala3.png")
+        self.imagembala = pygame.image.load("imagem/bala5.png")
 
-        self.rect = self.imagemBala.get_rect()
-        self.velocidadeBala = 5
+        self.rect = self.imagembala.get_rect()
+        self.velocidadebala = 5
         self.rect.top = posy
         self.rect.left = posx
 
     def trajetoria(self):
-        self.rect.top = self.rect.top - self.velocidadeBala
+        self.rect.top = self.rect.top - self.velocidadebala
 
     def colocar(self, superficie):
-        seperficie.blit(self.imagemBala, self.rect)
+        superficie.blit(self.imagembala, self.rect)
 
-class naveEspacial(pygame.sprite.Sprite):
+        
+
+class naveespacial(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.imagemNave = pygame.image.load("imagem/nave.png")
+        self.imagemnave = pygame.image.load("imagem/nave.png")
 
-        self.rect = self.imagemNave.get_rect()
-        self.rect.centerx = largura/2
+        self.rect = self.imagemnave.get_rect()
+        self.rect.centerx = largura / 2 
         self.rect.centery = altura - 60
 
-        self.lista.disparo = []
+        self.listadisparo = []
         self.vida = True
         self.velocidade = 20
 
-        def movimento(self):
-            if self.vida == True:
-                if self.rect.left <= 0:
-                    self.rect.left = 0
+    def movimento(self):
+        if self.vida == True:
+            if self.rect.left <= 0:
+                self.rect.left = 0
 
-                elif self.rect.right > 900:
-                    self.rect.left = 900
+            elif self.rect.right > 1200:
+                self.rect.left = 1200
 
-        def disparar(self, x, y):
-            minhaBala = Bala(x,y)
-            self.listaDisparo.append(minhaBala)
-            print("disparou")
+    def disparar(self, x, y):
+        minhabala = bala(x,y)
+        self.listadisparo.append(minhabala)
+        print("disparou")
 
-        def colocar(self, superficie):
-            superficie.blit(self.imagemNave, self.rect)
+    def colocar(self, superficie):
+        superficie.blit(self.imagemnave, self.rect)
 
 
-def invasaoEspaco():
+def invasaoespaco():
     pygame.init()
     tela = pygame.display.set_mode((largura, altura))
     pygame.display.set_caption("Invasão do Espaço")
-
-    jogador = naveEspacial()
-    ImagemFundo = pygame.image.load("imagem/galaxy2.png")
+    jogador = naveespacial()
+    imagemfundo = pygame.image.load("imagem/galaxy2.png")
     jogando = True
 
-    balaProjetil =Bala(largura/ 2, altura - 60)
+    balaprojetil = bala(largura/ 2, altura - 60)
 
     while True:
         jogador.movimento()
-        balaProjetil.trajetoria()
+        balaprojetil.trajetoria()
         for evento in pygame.event.get():
-            if evento.type ==   QUIT:
+            if evento.type == QUIT:
                 pygame.quit()
                 sys.exit()
 
@@ -78,21 +79,25 @@ def invasaoEspaco():
 
                 elif evento.key == K_SPACE:
                     x,y = jogador.rect.center
-                    jogador.disparo(x,y)
+                    jogador.disparar(x,y)
                     
                     
-        tela.blit(ImagemFundo, (0,0))
-        balaProjetil.colocar(tela)
+        tela.blit(imagemfundo, (0,0))
+        balaprojetil.colocar(tela)
         jogador.colocar(tela)
-        if len(jogador.listaDisparo) > 0:
-            for x in jogador.listaDisparo:
+        if len(jogador.listadisparo) > 0:
+            for x in jogador.listadisparo:
                 x.colocar(tela)
                 x.trajetoria()
+                if x.rect.top < 100:
+                    jogador.listadisparo.remove(x)
         pygame.display.update()
 
-invasaoEspaco()
+invasaoespaco()
         
         
+
+
 
         
 
